666a9d57580e2f719e5626872026d1cc36a885c0
Ricardo Ruy <ricardo.rruy@hotmail.com>
2023-07-16 01:31:48 -0300

    create Book component

---
+++ src/components/Book.jsx
@@ -0,0 +1,64 @@
+import {
+    Box,
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    Flex,
+    Heading,
+    Text,
+    CardFooter,
+  } from '@chakra-ui/react';
+  import { ArrowForwardIcon } from '@chakra-ui/icons';
+  
+  export const Book = ({
+    title = 'Book',
+    description = 'View a summary of all your customers over the last month.',
+    author = 'Author',
+    contributor = 'Contributor',
+    publisher = 'Publisher',
+    viewMoreAction,
+  }) => (
+    <Card variant="filled">
+      <Flex gap="12px" padding="8px" alignItems="center">
+        <Box
+          backgroundColor="blue.200"
+          color="blue.50"
+          width="150px"
+          minWidth="150px"
+          height="230px"
+          padding="6px 16px"
+          borderTop="20px solid"
+          borderColor="blue.300"
+        >
+          <Text as="b" noOfLines={2} fontSize="xs">
+            {title}
+          </Text>
+          <Text fontSize="xs">{author}</Text>
+        </Box>
+        <Box>
+          <CardHeader>
+            <Heading as="h3" size="md">
+              {title.toUpperCase()}
+            </Heading>
+            <Text fontSize="sm">
+              {author} | {publisher}
+            </Text>
+            <Text fontSize="xs">{contributor}</Text>
+          </CardHeader>
+          <CardBody>
+            <Text>{description}</Text>
+          </CardBody>
+          <CardFooter>
+            <Button
+              rightIcon={<ArrowForwardIcon />}
+              colorScheme="teal"
+              variant="outline"
+            >
+              View more
+            </Button>
+          </CardFooter>
+        </Box>
+      </Flex>
+    </Card>
+  );
