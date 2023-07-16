9af6aec0265ea3ac9d00154f97eb103cccb9245a
Ricardo Ruy <ricardo.rruy@hotmail.com>
2023-07-16 01:14:29 -0300

    add env vars to next configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    API_KEY: 'c5qsIcf9Yj6lyoeEfvqUsbttT5rcASwA',
+    API_BASE_URL: 'https://api.nytimes.com/svc/books/v3/lists',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
